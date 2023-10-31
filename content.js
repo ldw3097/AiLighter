60ca8606060fc66a132aaff4032637c30a666e14
hyeonuk <wjdanfdl11@naver.com>
2023-10-31 17:42:13 +0900

    hyeonuk:modify highlighting

--- content.js
+++ content.js
@@ -1,44 +1,83 @@
-let searchStrings;
-var regex = /\(|\)/g;
-var processedStrings;
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-  if (request.action === 'getValue') {
-    chrome.storage.local.get('key', function(result) {
-      searchStrings = JSON.parse(result.key);
-      processedStrings = searchStrings.map(function (str) {
-        return str.replace(regex, function (match) {
-          return "\\" + match;
-        });
-      });
-      processTextNodes(document.body);
-      a = false;
-    });
-  }
+const serverURL = "http://127.0.0.1:8000/";
+const url = document.URL;
 
-  if(request.action === 'reset')
-  {
-    var highlightArr = document.querySelectorAll(".highlighted");
-    console.log("asd" + highlightArr.length);
-    for(var i = 0; i < highlightArr.length; i++)
-    {
-      highlightArr[i].classList.remove("highlighted");
+var processedStrings;
+
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    if (request.action === 'reset') {
+        var highlightArr = document.querySelectorAll(".highlighted");
+        for (var i = 0; i < highlightArr.length; i++) {
+            if (highlightArr[i].parentNode) {
+                highlightArr[i].parentNode.replaceChild(document.createTextNode(highlightArr[i].textContent), highlightArr[i]);
+            }
+        }
     }
-  }
+
+    if (request.action === 'highlight') {
+        processedStrings = request.content;
+        console.log(processedStrings);
+        removeNbsp();
+        DeleteBanner();
+    }
 });
-function processTextNodes(node) {
-  if (node.nodeType === Node.TEXT_NODE) {
-    var text = node.textContent;
-    processedStrings.forEach(function (searchString) {
-      var searchText = new RegExp(searchString, 'g');
-      if (searchText.test(text)) {
-        var span = document.createElement('span');
-        span.innerHTML = text.replace(searchText, '<span class ="highlighted">$&</span>');
-        node.parentNode.replaceChild(span, node);
-      }
-    });
-  } else if (node.nodeType === Node.ELEMENT_NODE) {
-    for (var i = 0; i < node.childNodes.length; i++) {
-      processTextNodes(node.childNodes[i]);
+function removeNbsp() {
+    var elements = document.getElementsByTagName('*');
+    for (var i = 0; i < elements.length; i++) {
+        var element = elements[i];
+        if (element.childNodes.length > 0) {
+            element.innerHTML = element.innerHTML.replace(/&nbsp;/g, ' ');
+        }
     }
-  }
+    SpanAdd();
+    processTextNodes(document.body);
+}
+function processTextNodes(node) {
+    if (node.nodeType === Node.TEXT_NODE) {
+        var text = node.textContent;
+        var highlightedText = text; // 하이라이트 처리된 텍스트
+        processedStrings.forEach(function(searchString) {
+            if (searchString != "" && highlightedText.includes(searchString)) {
+                highlightedText = highlightedText.replace(searchString, '<span class="highlighted">$&</span>');
+            }
+        });
+        var span = document.createElement('span');
+        span.innerHTML = highlightedText;
+        if (node.parentNode) {
+            node.parentNode.replaceChild(span, node);
+        }
+    } else if (node.nodeType === Node.ELEMENT_NODE) {
+        for (var i = 0; i < node.childNodes.length; i++) {
+            processTextNodes(node.childNodes[i]);
+        }
+    }
+}
+
+function SpanAdd() 
+{
+    var item = document.querySelectorAll('.wrap_item');
+    for(var i = 0; i < item.length; i++)
+    {
+        var str = "";
+        var node = item[i].querySelectorAll('span');
+        if(node.length > 0)
+        {
+            for(var j = 0; j < node.length; j++)
+            {
+                str += node[j].textContent;
+            }
+            var span = document.createElement('span');
+            span.innerHTML = str;
+            item[i].innerHTML = '';
+            item[i].appendChild(span);
+        }
+    }
+    
+}
+function DeleteBanner() 
+{
+    var banner = document.querySelectorAll('.sticky');
+    for(var i = 0; i < banner.length; i++)
+    {
+        banner[i].parentNode.removeChild(banner[i]);
+    }
 }
